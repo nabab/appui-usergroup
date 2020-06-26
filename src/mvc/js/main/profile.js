f5e4a82ba8adab7108d791b4f03318f07b5a0fe4
Thomas Nabet <thomas.nabet@gmail.com>
2020-06-26 13:15:24 +0200

    Fixed the change of theme

--- src/mvc/js/main/profile.js
+++ src/mvc/js/main/profile.js
@@ -23,21 +23,22 @@
       data: {
         deep: true,
         handler(){
+          const st = '.less,/01-basic.less,/02-background.less';
           bbn.fn.log("DIRTY HANDLER", this.theme, this.source.schema.theme, this.$refs.form.source[this.source.schema.theme]);
           if ( this.theme !== this.$refs.form.source[this.source.schema.theme] ){
+            bbn.fn.log("YEAD");
             let lnks = document.head.getElementsByTagName('link');
             let url = false;
             bbn.fn.each(lnks, a => {
-            	bbn.fn.log(a.href);
               if (
                 (a.rel === 'stylesheet')
                 && a.href
-                && a.href.match(this.theme + '.less,01-basic.less,02-background.less')
+                && a.href.match(this.theme + st)
               ){
                 bbn.fn.log("CSS FOUND");
                 a.href = bbn.fn.replaceAll(
-                  this.theme + '.less,01-basic.less,02-background.less',
-                  this.$refs.form.source[this.source.schema.theme] + '.less,01-basic.less,02-background.less',
+                  this.theme + st,
+                  this.$refs.form.source[this.source.schema.theme] + st,
                   a.href
                 );
                 this.theme = this.$refs.form.source[this.source.schema.theme];
