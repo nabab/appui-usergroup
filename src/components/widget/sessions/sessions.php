172af94b1202a24ec1cccc14987700f1cbd9b7cb
Thomas Nabet <thomas.nabet@gmail.com>
2020-09-22 20:19:36 +0200

    Better looking users list

--- src/components/widget/sessions/sessions.php
+++ src/components/widget/sessions/sessions.php
@@ -1,36 +1,36 @@
-<div class="bbn-padded">
-  <div class="bbn-w-100">
-    <div class="bbn-header bbn-b bbn-spadded bbn-no-border-bottom"><?=_('OPENED')?></div>
-    <div class="bbn-w-100">
-      <bbn-table :source="source.opened" :scrollable="false">
-        <bbns-column title="<?=_('Created')?>"
-                     field="creation"
-                     type="datetime"
-                     cls="bbn-c"
-        ></bbns-column>
-        <bbns-column title="<?=_('Last activity')?>"
-                     field="last_activity"
-                     type="datetime"
-                     cls="bbn-c"
-        ></bbns-column>
-      </bbn-table>
-    </div>
-  </div>
-  <div class="bbn-top-space bbn-w-100">
-    <div class="bbn-header bbn-b bbn-spadded bbn-no-border-bottom"><?=_('CLOSED')?></div>
-    <div class="bbn-w-100">
-      <bbn-table :source="source.closed" :scrollable="false">
-        <bbns-column title="<?=_('Created')?>"
-                     field="creation"
-                     type="datetime"
-                     cls="bbn-c"
-        ></bbns-column>
-        <bbns-column title="<?=_('Last activity')?>"
-                     field="last_activity"
-                     type="datetime"
-                     cls="bbn-c"
-        ></bbns-column>
-      </bbn-table>
-    </div>
+<div class="bbn-w-100 bbn-hpadded bbn-top-padded">
+  <div class="bbn-rel bbn-w-100">
+    <div class="bbn-w-100 bbn-header bbn-b bbn-spadded bbn-no-border-bottom"><?=_('OPENED')?></div>
+    <bbn-table class="bbn-w-100"
+                :source="source.opened"
+                :scrollable="false"
+    >
+      <bbns-column title="<?=_('Created')?>"
+                    field="creation"
+                    type="datetime"
+                    cls="bbn-c"
+      ></bbns-column>
+      <bbns-column title="<?=_('Last activity')?>"
+                    field="last_activity"
+                    type="datetime"
+                    cls="bbn-c"
+      ></bbns-column>
+    </bbn-table>
+    <div class="bbn-top-space bbn-w-100 bbn-header bbn-b bbn-spadded bbn-no-border-bottom"><?=_('CLOSED')?></div>
+    <bbn-table class="bbn-w-100"
+                :source="source.closed"
+                :scrollable="false"
+    >
+      <bbns-column title="<?=_('Created')?>"
+                    field="creation"
+                    type="datetime"
+                    cls="bbn-c"
+      ></bbns-column>
+      <bbns-column title="<?=_('Last activity')?>"
+                    field="last_activity"
+                    type="datetime"
+                    cls="bbn-c"
+      ></bbns-column>
+    </bbn-table>
   </div>
 </div>
