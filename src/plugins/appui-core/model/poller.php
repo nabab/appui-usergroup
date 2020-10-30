c322d44b79e88c350a60e29f125c111e6b9ecad3
Thomas Nabet <thomas.nabet@gmail.com>
2020-10-30 04:31:13 +0100

    Updated menu config file

--- src/plugins/appui-core/model/poller.php
+++ src/plugins/appui-core/model/poller.php
@@ -3,7 +3,6 @@ return [[
   'id' => 'appui-usergroup-0',
   'frequency' => 10,
   'function' => function(array $data) use($model){
-    $ucfg = $model->inc->user->get_class_cfg();
     $model->inc->user->update_activity();
     return ['success' => true];
   }
