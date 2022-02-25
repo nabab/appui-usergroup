0ef04d411164f52fc604b0880a52dc7b538d3060
nabab <thomas.nabet@gmail.com>
2022-02-25 16:12:20 +0100

    Bugfix in clean_sessions

--- src/cli/clean_sessions.php
+++ src/cli/clean_sessions.php
@@ -5,26 +5,25 @@
  **/
 use bbn\X;
  /** @var int $delay */
-$delay = 7*3600*24;
+$delay = 12*3600*24;
 
 /** @var $ctrl \bbn\Mvc\Controller */
 echo (int)$ctrl->db->delete('bbn_users_sessions', [
   'id_user' => null,
-  ['last_activity', '<', date('Y-m-d H:i:s', time() - $delay)]
-])." anonymous sessions rows deleted.".PHP_EOL;  
+  [['last_activity', '<', date('Y-m-d H:i:s', time() - $delay)]]
+])." anonymous sessions rows deleted.".PHP_EOL;
 
 $users = $ctrl->db->rselectAll([
   'tables' => ['bbn_users_sessions'],
   'fields' => ['id_user', 'num' => 'COUNT(id)', 'max' => 'MAX(last_activity)'],
-  'where' => ['last_activity', '<', date('Y-m-d H:i:s', time() - $delay)],
+  'where' => [['last_activity', '<', date('Y-m-d H:i:s', time() - $delay)]],
+  'having' => [['num', '>', 1]],
   'group_by' => ['id_user']
 ]);
 
 foreach ($users as $u) {
-  if ($u['num'] > 1) {
-    echo (int)$ctrl->db->delete('bbn_users_sessions', [
-      'id_user' => $u['id_user'],
-      'last_activity', '<', $u['max']
-    ])._("Sessions rows deleted for user").' '.$u['id_user'].PHP_EOL;
-  }
+  echo (int)$ctrl->db->delete('bbn_users_sessions', [
+    'id_user' => $u['id_user'],
+    ['last_activity', '<', $u['max']]
+  ])._("Sessions rows deleted for user").' '.$u['id_user'].PHP_EOL;
 }
