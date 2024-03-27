a8eb224bfc254eeae7e2a98eafb093ef41af817f
nabab <thomas.nabet@gmail.com>
2024-03-27 02:52:27 +0100

    bbn-cp

--- src/mvc/html/main.php
+++ src/mvc/html/main.php
@@ -4,16 +4,16 @@
             :nav="true">
   <bbns-container url="profile"
            title="<?= _("Informations") ?>"
-           :static="true"
+           :fixed="true"
            :load="true"/>
   <bbns-container url="password"
            title="<?= _("Password") ?>"
-           :static="true"
+           :fixed="true"
            :load="true"/>
   <bbns-container v-if="hasNotifications"
                   url="notifications"
                   title="<?= _("Notifications") ?>"
-                  :static="true"
+                  :fixed="true"
                   :load="true"
                   component="appui-notification-settings"/>
 </bbn-router>
